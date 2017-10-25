82cb1870a4ae884b5c111f9f9711c417bb900984
David Zhang <davzee@hotmail.com>
2017-10-24 20:28:54 -0700

    Clean up

--- map_reduce.py
+++ map_reduce.py
@@ -1,8 +1,14 @@
+import multiprocessing as mp
+
+# This base class supports reducer multiprocessing through constructor flag.
+# The flag can be set either using default args or through subclass explicitly enabling it.
 class MapReduce:
-    def __init__(self, lines, output=False, mappings=None):
+    def __init__(self, lines, output=False, mappings=None, multiprocessing=False):
+        self.manager = mp.Manager()
         self.mappings = {} if not mappings else mappings
-        self.results = {}
+        self.results = self.manager.dict()  # Thread-safe dictionary
         self.output = output
+        self.multiprocessing = multiprocessing
 
         for line in lines:
             mapper = self.mapper(line)
@@ -13,18 +19,28 @@ class MapReduce:
                     self.mappings[key] = []
                 self.mappings[key].append(val)
 
-        for key in self.mappings:
-            reducer = self.reducer(key, self.mappings[key])
-            if not reducer:
-                break
-            self.results[reducer[0]] = reducer[1]
+        for key in self.mappings.keys():
+            if self.multiprocessing:
+                p = mp.Process(target=self.__generate_results, args=(key,))
+                p.start()
+                p.join()
+            else:
+                self.__generate_results(key)
 
         if self.output:
-            for key in self.results:
+            for key in self.results.keys():
                 print key, self.results[key]
 
+    def __generate_results(self, key):
+        reducer = self.reducer(key, self.mappings[key])
+        if not reducer:
+            return
+        self.results[reducer[0]] = reducer[1]
+
+    # Overridden by subclass
     def mapper(self, line):
         return None
 
+    # Overriden by subclass
     def reducer(self, key, entries):
         return None
